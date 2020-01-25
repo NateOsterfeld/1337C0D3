2e0280c9811ec228461564ac20fc860ae1cae1f0
Nate <nawsterfeld@outlook.com>
2020-01-25 17:13:30 -0500

    add alt solution to rotateString

--- easy/rotateString/solution.js
+++ easy/rotateString/solution.js
@@ -1,31 +1,23 @@
 var rotateString = function(A, B) {
-	if (!A.length && !B.length) return true
-
-	for (i of A) {
-		A = [...A].splice(1).concat(A[0])
-		if (A.join('') == B) return true
-	}
-
-	return false
+    for (let i = 0; i < A.length; i++)
+        if (A.slice(i) + A.slice(0, i) === B)
+            return true
+    
+    return false
 }
 
 console.log(rotateString('abcde', 'cdeab')) // true
 console.log(rotateString('abcde', 'abcded')) // false
 module.exports = rotateString
 
-// alt solution - abstracts shifting logic into an isolated function
+// alt solution
 // var rotateString = function(A, B) {
-// 	if (A.length !== B.length) return false
 // 	if (!A.length && !B.length) return true
 
-// 	for (i = 0; i < A.length; i++) {
-// 		A = shift(A)
+// 	for (i of A) {
+// 		A = [...A].splice(1).concat(A[0])
 // 		if (A.join('') == B) return true
 // 	}
 
 // 	return false
 // }
-
-// function shift(iter) {
-// 	return [...iter].splice(1).concat(iter[0])
-// }    
