aa5a489ac31b397adffd9f91828e2a3b4b6cf1cc
Nate <nawsterfeld@outlook.com>
2020-01-30 14:02:51 -0500

    update 25PercentSorted (explanation)

--- easy/25PercentSorted/solution.js
+++ easy/25PercentSorted/solution.js
@@ -11,3 +11,7 @@ var findSpecialInteger = function(arr) {
 		if (freq[i] > arr.length / 4) return i
 	}
 }
+
+// Explanation: So all we're really doing is storing each number in our array in a map (called 'freq'), and incrementing
+// the value for that number by 1 every time it comes up.Once one of our numbers can be seen to have come up 25 % of the time,
+// by checking if it is greater than our array's length / 4, we know to go ahead and return that number immediately after.
