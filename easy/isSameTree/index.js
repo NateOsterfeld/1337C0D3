ca5daf8fa96e939a712ef3b52234a27253ebd74b
Nate <nawsterfeld@outlook.com>
2020-02-06 23:22:26 -0500

    add isSameTree

---
+++ easy/isSameTree/index.js
@@ -0,0 +1,16 @@
+/* Same Tree
+Given two binary trees, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+*/
+ function TreeNode(val) {
+		this.val = val
+		this.left = this.right = null
+ }
+
+ /**
+  * @param {TreeNode} p
+  * @param {TreeNode} q
+  * @return {boolean}
+  */
+ var isSameTree = function(p, q) {
+ }
