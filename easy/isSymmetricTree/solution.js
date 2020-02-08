5f2a0f4e002c6a4af058cf5c73290424ff1d5c0b
Nate <nawsterfeld@outlook.com>
2020-02-08 14:18:36 -0500

    add isSymmetricTree

---
+++ easy/isSymmetricTree/solution.js
@@ -0,0 +1,62 @@
+/* Symmetric Tree
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+*/
+
+function Node(val) {
+	this.val = val
+	this.left = this.right = null
+}
+
+/**
+ * @param {Node} root
+ * @return {boolean}
+ */
+const isSymmetric = root => {
+	if (!root) return true
+
+	const mirror = (l, r) => {
+		if (!l && !r) return true
+		if (!l || !r) return false
+
+		return (
+			l.val === r.val &&
+			mirror(l.left, r.right) &&
+			mirror(l.right, r.left)
+		)
+	}
+
+	return mirror(root.left, root.right)
+}
+
+const n = new Node(1)
+n.left = new Node(2)
+n.right = new Node(2)
+n.left.left = new Node(3)
+n.left.right = new Node(4)
+n.right.left = new Node(4)
+n.right.right = new Node(3)
+console.log(isSymmetric(n)) // true
+
+const n2 = new Node(1)
+n2.left = new Node(2)
+n2.right = new Node(2)
+n2.left.right = new Node(3)
+n2.right.right = new Node(3)
+console.log(isSymmetric(n2)) // false
