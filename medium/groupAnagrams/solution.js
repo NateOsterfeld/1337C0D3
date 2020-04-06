6dd380e783b9e9491a686c271ab158d73f10b565
NateOsterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2020-04-06 05:35:53 -0400

    update groupAnagrams w/ alternate solution

--- medium/groupAnagrams/solution.js
+++ medium/groupAnagrams/solution.js
@@ -28,3 +28,21 @@ var groupAnagrams = function(strs) {
 
     return Object.values(map)
 }
+
+var groupAnagrams = function(strs) {
+    let map = new Map()
+    
+    for(str of strs) {
+        let key = 0;
+        for (let char of str) {
+            const i = char.charCodeAt(0) 
+            key += i**4
+        }
+
+        !map.has(key) 
+            ? map.set(key, [str])
+            : map.set(key, map.get(key).concat(str))
+    }
+
+    return Array.from(map.values())
+};
